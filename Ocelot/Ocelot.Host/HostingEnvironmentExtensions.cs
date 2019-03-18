b534442932d0c2068a72112d3a05b9e29146ac4a
ShawnWong <wangxf3@rich-healthcare.com>
2019-03-18 15:44:02 +0800

    初始化

---
+++ Ocelot/Ocelot.Host/HostingEnvironmentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.Core;
+
+namespace Ocelot.Host
+{
+    public static class HostingEnvironmentExtensions
+    {
+        public static IConfigurationRoot GetAppConfiguration(this IHostingEnvironment env)
+        {
+            return AppConfigurations.Get(env.ContentRootPath, env.EnvironmentName, env.IsDevelopment());
+        }
+    }
+}
---
+++ Ocelot/Ocelot.Host/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.Host
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+        private readonly IHostingEnvironment _env;
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = _env.GetAppConfiguration();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddOcelot(_env.GetAppConfiguration());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseOcelot().Wait();
+            app.UseMvc();
+
+        }
+    }
+}
